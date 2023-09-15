dffe086630e671f99297c990e718b1d388cf2d55
Courtney Koko <courtney3koko@yahoo.com>
2023-09-15 12:04:35 -0500

    Add Images for main Banner

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,35 +3,38 @@ import HeadComponent from "../components/Head";
 import MainBanner from "../components/Content/Banners/MainBanner";
 import BannerImage from "../public/assets/images/home/CarHug.png";
 import PATHCONSTANTS from '../constants/sitemap';
-
+import MotorcycleIcon from "../public/assets/images/home/icons/motorcycle.png"
+import CarIcon from "../public/assets/images/home/icons/car.png"
+import HomeIcon from "../public/assets/images/home/icons/house.png"
+import RentersIcon from "../public/assets/images/home/icons/rent.png"
 const bannerContent = {
   mainHeading: "Insurance Savings the Size of Texas",
-  subHeading: "Your One-Stop Insurance Destination: Fast, Easy, and Affordable Coverages for All Your Needs",
+  subHeading: "A Better Tomorrow Starts Today",
   image: BannerImage,
   ctaItems: [
     {
       mainText: "Auto",
       subText: "Switch and save an average of $247",
       link: PATHCONSTANTS.QUOTES.AUTO,
-      icon: null
+      icon: CarIcon
     },
     {
       mainText: "Home",
       subText: "Save up to 40% on home insurance",
       link: PATHCONSTANTS.QUOTES.HOME,
-      icon: null
+      icon: HomeIcon
     },
     {
       mainText: "Renters",
       subText: "Get renters insurance for as low as $5/month",
       link: PATHCONSTANTS.QUOTES.RENTERS,
-      icon: null
+      icon: RentersIcon
     },
     {
       mainText: "Motorcycle",
       subText: "Get motorcycle insurance for as low as $75/year",
       link: PATHCONSTANTS.QUOTES.MOTORCYCLE,
-      icon: null
+      icon: MotorcycleIcon
     }
   ],
   ctaButton: {
