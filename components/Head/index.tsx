adc56cf6a24496673f6cb34e425c8083309bd10e
Courtney Koko <courtney3koko@yahoo.com>
2023-11-24 14:05:48 -0600

    Favicon

--- components/Head/index.tsx
+++ components/Head/index.tsx
@@ -8,9 +8,9 @@ type HeadComponentPropsType = {
 const HeadComponent = ({ title, metaData }: HeadComponentPropsType) => {
   return (
     <Head>
-      <title>{title}</title>
-      <meta name="description" content={metaData} />
-      <link rel="icon" href="/favicon.ico" />
+      <title>{title || "Statewide Insurance"}</title>
+      <meta name="description" content={metaData || "Statewide Insurance provides you with insurance coverage options, benefits and discounts at a price you can afford."} />
+      <link rel="icon" href="https://firebasestorage.googleapis.com/v0/b/insurance-hub-397016.appspot.com/o/sw-favicon.ico?alt=media&token=ac2dfddc-b5e2-4dae-9eab-14d602b002dc" />
       <meta name="google-site-verification" content="eBcP0RUizk4nbWiQlzGJKWaFZaPuYTm9Fzo2S954WJE" />
     </Head>
   )
