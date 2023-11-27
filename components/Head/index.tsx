a766ea652b9505c97369e3944bfd524f4e57f56e
Courtney Koko <courtney3koko@yahoo.com>
2023-11-27 08:17:23 -0600

    Updatte creator and publisher

--- components/Head/index.tsx
+++ components/Head/index.tsx
@@ -9,6 +9,8 @@ const HeadComponent = ({ title, metaData }: HeadComponentPropsType) => {
   return (
     <Head>
       <title>{title || "Statewide Insurance"}</title>
+      <meta name="creator" content="Statewide Insurance" />
+      <meta name="publisher" content="Statewide Insurance" />
       <meta name="description" content={metaData || "Statewide Insurance provides you with insurance coverage options, benefits and discounts at a price you can afford."} />
       <link rel="icon" href="https://firebasestorage.googleapis.com/v0/b/insurance-hub-397016.appspot.com/o/sw-favicon.ico?alt=media&token=ac2dfddc-b5e2-4dae-9eab-14d602b002dc" />
       <meta name="google-site-verification" content="eBcP0RUizk4nbWiQlzGJKWaFZaPuYTm9Fzo2S954WJE" />
